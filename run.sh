ac0c963969bb0c88e26d0ac33fe315f3b9778d4d
tquin <49335547+tquin@users.noreply.github.com>
2024-03-19 22:02:24 +1100

    Add -v option to run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,20 @@
 set -e
 
 for ARGUMENT in "$@"; do
-    if [[ "$ARGUMENT" = "--upgrade" || "$ARGUMENT" = "-f" ]]; then
+    if [[ "$ARGUMENT" = "--upgrade" || "$ARGUMENT" = "--force" || "$ARGUMENT" = "-f" ]]; then
         # -f (force) will re-download latest collections/roles
-        GALAXY_REQS_ARGS="-f" 
+        GALAXY_REQS_ARGS="-f"
+    fi
+
+    if [[ "$ARGUMENT" = "--verbose" || "$ARGUMENT" = "-v" ]]; then
+        PLAYBOOK_VERBOSE_LEVEL="-v"
+    else
+        PLAYBOOK_VERBOSE_LEVEL=""
     fi
 done
 
+echo "PLAYBOOK_VERBOSE_LEVEL: $PLAYBOOK_VERBOSE_LEVEL"
+
 ansible-galaxy install -r requirements.yaml $GALAXY_REQS_ARGS
 sudo ansible-playbook setup.yaml
-ansible-playbook run.yaml
+ansible-playbook $PLAYBOOK_VERBOSE_LEVEL run.yaml
